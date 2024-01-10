c2c566cc0f11b10a12dd762d04ca905dce05a49b
Al Tsaplin <al.tsaplin@gmail.com>
2024-01-10 11:41:41 +0300

    Fix (gulpfile & package) Setup for working further

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 const { src, dest, watch, parallel, series } = require('gulp');
 
-const scss = require('gulp-sass');
-const concat = require('gulp-concat');
-const browserSync = require('browser-sync').create();
-const uglify = require('gulp-uglify-es').default;
-const autoprefixer = require('gulp-autoprefixer');
-const imagemin = require('gulp-imagemin');
-const del = require('del');
+const scss = require('gulp-sass')(require('sass')),
+  concat = require('gulp-concat'),
+  autoprefixer = require('gulp-autoprefixer'),
+  uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin'),
+  del = require('del'),
+  browserSync = require('browser-sync').create();
 
 function browsersync() {
   browserSync.init({
@@ -17,70 +17,78 @@ function browsersync() {
   });
 }
 
-function cleanDist() {
-  return del('dist');
+function styles() {
+  return (
+    src('app/scss/**/*.scss')
+      .pipe(scss({ outputStyle: 'compressed' }))
+      .pipe(concat("style.min.css"))
+      .pipe(
+        autoprefixer({
+          overrideBrowserslist: ['last 10 versions'],
+          grid: true,
+        })
+      )
+      .pipe(dest('app/css'))
+      .pipe(browserSync.stream())
+  );
+}
+
+function scripts() {
+  return src('app/js/main.js')
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js/'))
+    .pipe(browserSync.stream());
 }
 
 function images() {
   return src('app/images/**/*')
-    .pipe(
-      imagemin([
+    .pipe(imagemin([
         imagemin.gifsicle({ interlaced: true }),
         imagemin.mozjpeg({ quality: 75, progressive: true }),
         imagemin.optipng({ optimizationLevel: 5 }),
         imagemin.svgo({
-          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+          plugins: [
+            { removeViewBox: true }, 
+            { cleanupIDs: false }
+          ],
         }),
       ])
     )
-    .pipe(dest('dist/images'));
-}
-// here I'm adding additional downloaded libraries into src([''])
-function scripts() {
-  return src(['node_modules/jquery/dist/jquery.js', 'app/js/main.js'])
-    .pipe(concat('main.min.js'))
-    .pipe(uglify())
-    .pipe(dest('app/js'));
-}
-
-function styles() {
-  return src('app/scss/style.scss')
-    .pipe(scss({ outputStyle: 'compressed' }))
-    .pipe(concat('style.min.css'))
-    .pipe(
-      autoprefixer({
-        overrideBrowserslist: ['last 10 version'],
-        grid: true,
-      })
-    )
-    .pipe(dest('app/css'))
-    .pipe(browserSync.stream());
+    .pipe(dest('docs/images'));
 }
 
 function build() {
-  return src(
-    [
-      'app/css/style.min.css',
-      'app/fonts/**/*',
-      'app/js/main.min.js',
-      'app/*.html',
-    ],
-    { base: 'app' }
-  ).pipe(dest('dist'));
+  return src([
+    'app/**/*.html', 
+    'app/css/style.min.css', 
+    'app/js/main.min.js',
+    'app/favicons/**',
+    'app/fonts/**/*'
+  ], 
+  {
+    base: 'app',
+  })
+    .pipe(dest('docs'));
+}
+
+function cleanDocs() {
+  return del('docs');
 }
 
 function watching() {
-  watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts); //"!" means excluded, otherwise it be looped when watching is active
-  watch(['app/*.html']).on('change', browserSync.reload);
+  watch(['app/scss/*.scss'], styles);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/**/*.html']).on('change', browserSync.reload);
 }
 
 exports.styles = styles;
-exports.watching = watching;
-exports.browsersync = browsersync;
 exports.scripts = scripts;
+exports.browsersync = browsersync;
+exports.watching = watching;
 exports.images = images;
-exports.cleanDist = cleanDist;
+exports.cleanDocs = cleanDocs;
 
-exports.build = series(cleanDist, images, build);
 exports.default = parallel(styles, scripts, browsersync, watching);
+
+exports.build = series(cleanDocs, images, build);
